83d04c0730c37532d2617569a9e786c9e6cd3cdc
LiaoZhimin <876100193@qq.com>
2020-12-29 14:12:32 +0800

    优化models和admin

--- LYNews/app01/admin.py
+++ LYNews/app01/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from models import Category, Tags, Article
+
 
 # Register your models here.
+@admin.register(Article)  # 绑定模型到表格
+class  ArticleAdmin(admin.ModelAdmin):  
+    list_display = ('id','name', 'category', 'tags', 'excerpt','body', 'views','photo','created_time')  # 表格显示的列，model的属性
+    list_per_page = 50  # list_per_page设置每页显示多少条记录，默认是100条
+    ordering = ('-id',)  # ordering设置默认排序字段，负号表示降序排序
+    # list_editable 设置默认可编辑字段，在列表里就可以编辑
+    list_editable = [ 'name', 'category', 'tags', 'excerpt','body']     
+    # fk_fields = ['category']  # fk_fields 设置显示外键字段
+    list_display_links = ('id')  # 设置哪些字段可以点击进入编辑界面
+    search_fields = ['name', 'category', 'tags', 'excerpt','body']  # 搜索栏
+    list_filter = ['category', 'tags']  # 右侧筛选
+    date_hierarchy = 'created_time'  # 日期筛选属性
+
+
+admin.site.site_header = '管理后台'
+admin.site.site_title = 'LYNews--Happy Eary Day'
+
--- LYNews/app01/models.py
+++ LYNews/app01/models.py
@@ -18,21 +18,21 @@ class Tags(models.Model):
 
 class Article(models.Model):
     """分类"""
-    name = models.CharField(max_length=100,null=False)
+    name = models.CharField("名称",max_length=100,null=False)
     # db_index=False 是否为该参数创建索引，默认值：default =， max_length =30 限制字符长度 
     # primary_key = False 主键，一般和AutoField()一起使用，unique=False 是否唯一值
-    sex = models.BooleanField('性别',choices=[(True,'男'),(False,'女'),],default=False) # 布尔字段，值为True或False
-    points = models.DecimalField(max_digits=8, decimal_places=2) # 十进制浮点数，共8位其中2位小数
-    points2= models.FloatField()
-    body = models.TextField()
-    created_time = models.DateTimeField(auto_now=False, auto_now_add=False)
-    excerpt = models.CharField(max_length=200, blank=True)  # 文章摘要，可为空
+    # sex = models.BooleanField('性别',choices=[(True,'男'),(False,'女'),],default=False) # 布尔字段，值为True或False
+    # points = models.DecimalField(max_digits=8, decimal_places=2) # 十进制浮点数，共8位其中2位小数
+    # points2= models.FloatField()
+    body = models.TextField('内容')
+    created_time = models.DateTimeField('创建时间', auto_now=True, auto_now_add=False)
+    excerpt = models.CharField('摘要', max_length=200, blank=True)  # 文章摘要，可为空
     # ForeignKey表示1对多（多个post对应1个category）
     # category = models.ForeignKey(Category, on_delete=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='分类', default='1')
-    tags = models.ManyToManyField(Tags, blank=True)
-    views = models.PositiveIntegerField(default=0)  # 阅读量
-    photo = models.ImageField(null=True, upload_to = 'photos/%Y/%m/%d/')
+    tags = models.ManyToManyField(Tags, blank=True, verbose_name='标签')
+    views = models.PositiveIntegerField('阅读量',default=0)  # 阅读量
+    photo = models.ImageField('额外图片',null=True, upload_to = 'photos/%Y/%m/%d/')
     
     class Meta:
         verbose_name = '文章明细表'
